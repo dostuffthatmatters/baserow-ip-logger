b50aca3809944964f3817b033ab6c8d3ce4d904f
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-16 14:59:32 +0100

    Implement functionality

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+import json
+import os
 from pydantic import BaseModel
 import requests
+
+CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
 
 
 class Config(BaseModel):
@@ -28,7 +32,15 @@ def delete_row_ids(config: Config, row_ids: list[int]) -> list[int]:
     )
 
 
-def create_row(config: Config, local_ip: str, public_ip: str) -> list[int]:
+def get_local_ip() -> str:
+    return ""
+
+
+def get_public_ip() -> str:
+    return ""
+
+
+def create_row(config: Config) -> list[int]:
     requests.post(
         f"https://api.baserow.io/api/database/rows/table/{config.table_id}?user_field_names=true",
         headers={
@@ -37,7 +49,22 @@ def create_row(config: Config, local_ip: str, public_ip: str) -> list[int]:
         },
         json={
             "node-identifier": config.node_identifier,
-            "local-ip-address": local_ip,
-            "public-ip-address": public_ip,
+            "local-ip-address": get_local_ip(),
+            "public-ip-address": get_public_ip(),
         },
     )
+
+
+if __name__ == "__main__":
+    # load config from "config.json"
+    with open(CONFIG_PATH) as f:
+        config = Config(**json.load(f))
+
+    # get list of outdated entries
+    existing_row_ids = get_existing_row_ids(config)
+
+    # add new entry
+    create_row(config)
+
+    # remove existing rows after successful update
+    delete_row_ids(existing_row_ids)
